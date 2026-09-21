2cb31a852febb00f9c4e51cc47e1ddf46ad8c9d0
ZReka <zelenkar@kkszki.hu>
2026-09-21 02:19:22 -0700

    BlogPost Model second commit

---
+++ BlogApi/Controllers/BlogPostController.cs
@@ -0,0 +1,35 @@
+﻿using BlogApi.Models.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+
+namespace BlogApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogPostController : ControllerBase
+    {
+        public string ConnectionString = "server=localhost;uid=root;password=;database=blog;";
+
+        [HttpPost]
+        public object AddNewBlogger([FromBody] AddNewBloggerDto addNewBloggerDto)
+        {
+            var connection = new MySqlConnection(ConnectionString);
+            connection.Open();
+            string sql = @"INSERT INTO `blogpost`(`id`, `blog_id`, `title`, `content`, `created_at`) VALUES ('[value-1]','[value-2]','[value-3]','[value-4]','[value-5]')";
+
+            var cmd = new MySqlCommand(sql, connection);
+
+            cmd.Parameters.AddWithValue("@name", addNewBloggerDto.Name);
+            cmd.Parameters.AddWithValue("@email", addNewBloggerDto.Email);
+            cmd.Parameters.AddWithValue("@age", addNewBloggerDto.Age);
+            cmd.Parameters.AddWithValue("@password", addNewBloggerDto.Password);
+            cmd.Parameters.AddWithValue("@registrationtime", DateTime.Now);
+
+            cmd.ExecuteNonQuery();
+
+            connection.Close();
+            return new { massage = "Sikeres felvétel", result = addNewBloggerDto };
+        }
+    }
+}
--- BlogApi/obj/Debug/net10.0/BlogApi.AssemblyInfo.cs
+++ BlogApi/obj/Debug/net10.0/BlogApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf4292111fc697323da7f68c44f51f58116249ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
