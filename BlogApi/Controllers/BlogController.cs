bf4292111fc697323da7f68c44f51f58116249ac
ZReka <zelenkar@kkszki.hu>
2026-09-21 01:52:49 -0700

    BlogPost Model is Created

---
+++ BlogApi/Controllers/BlogController.cs
@@ -0,0 +1,153 @@
+﻿using BlogApi.Models;
+using BlogApi.Models.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+
+
+namespace BlogApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        public string ConnectionString = "server=localhost;uid=root;password=;database=blog;";
+
+        [HttpGet]
+        public List<Blogger> GetBloggers() 
+        {
+            List<Blogger> bloggers = new List<Blogger>();
+
+            var connection = new MySqlConnection(ConnectionString);
+
+            connection.Open();
+
+            string sql = "SELECT * FROM blogger;";
+
+            var cmd = new MySqlCommand(sql,connection);
+
+            var data = cmd.ExecuteReader();
+
+            while (data.Read())
+            {
+                var blogger = new Blogger
+                {
+                    Id = data.GetInt32("id"),
+                    Name = data.GetString("name"),
+                    Email = data.GetString("email"),
+                    Age = data.GetInt32("age"),
+                    Password=data.GetString("password"),
+                    RegistrationTime = data.GetDateTime("registrationTime")
+                };
+
+                bloggers.Add(blogger);
+            }
+
+            connection.Close();
+
+            return bloggers;
+        }
+
+        [HttpPost]
+        public object AddNewBlogger([FromBody] AddNewBloggerDto addNewBloggerDto)
+        {
+            var connection = new MySqlConnection(ConnectionString);
+            connection.Open();
+            string sql = @"INSERT INTO `blogger`( `Name`, `Email`, `Age`, `Password`, `RegistrationTime`)
+                VALUES (@name,@email,@age,@password,@registrationtime)";
+
+            var cmd=new MySqlCommand(sql,connection);
+
+            cmd.Parameters.AddWithValue("@name", addNewBloggerDto.Name);
+            cmd.Parameters.AddWithValue("@email", addNewBloggerDto.Email);
+            cmd.Parameters.AddWithValue("@age", addNewBloggerDto.Age);
+            cmd.Parameters.AddWithValue("@password", addNewBloggerDto.Password);
+            cmd.Parameters.AddWithValue("@registrationtime", DateTime.Now);
+
+            cmd.ExecuteNonQuery();
+
+            connection.Close();
+            return new {massage="Sikeres felvétel", result= addNewBloggerDto};
+        }
+        [HttpDelete]
+        public object DeleteBlogger(int id)
+        {
+            var connection = new MySqlConnection(ConnectionString);
+            connection.Open();
+
+            string sql = @"DELETE FROM `blogger` WHERE `id` = @id";
+
+            var cmd=new MySqlCommand(sql,connection);
+
+            cmd.Parameters.AddWithValue("@id", id);
+
+            cmd.ExecuteNonQuery ();
+
+            connection.Close();
+
+            return new { message = "Sikeres törles", result = "" };
+        }
+
+        [HttpPut]
+        public object UpdateBlogger([FromQuery] int id, UpdateBloggerDto updateBloggerDto)
+        {
+            var connection = new MySqlConnection(ConnectionString);
+            connection.Open ();
+
+            var sql = @"UPDATE `blogger` SET `Name`=@name,`Email`=@email,`Age`=@age,`Password`=@age,`RegistrationTime`=@registrationtime' WHERE `id` = @id;";
+
+            var cmd = new MySqlCommand(sql,connection);
+
+            cmd.Parameters.AddWithValue("@name", updateBloggerDto.Name);
+            cmd.Parameters.AddWithValue("@email", updateBloggerDto.Email);
+            cmd.Parameters.AddWithValue("@agee", updateBloggerDto.Age);
+            cmd.Parameters.AddWithValue("@password", updateBloggerDto.Password);
+            cmd.Parameters.AddWithValue("@nid", id);
+
+            cmd.ExecuteNonQuery();
+
+            connection.Close();
+
+            return new { massage = "Sikeres frissítés", result = updateBloggerDto };
+        }
+
+        [HttpGet("byId")]
+        public object GetBloggerById(int id)
+        {
+            var connection = new MySqlConnection(ConnectionString);
+            connection.Open();
+
+            string sql = @" SELECT * FROM `blogger` WHERE `id` = @id";
+
+            var cmd = new MySqlCommand(sql, connection);
+            cmd.Parameters.AddWithValue("@id", id);
+
+            var datereader = cmd.ExecuteReader();
+
+            object? data = null;
+
+            if (datereader.Read() == true)
+            {
+                var blogger = new Blogger
+                {
+                    Id = datereader.GetInt32("id"),
+                    Name = datereader.GetString("name"),
+                    Email = datereader.GetString("email"),
+                    Age = datereader.GetInt32("age"),
+                    Password = datereader.GetString("password"),
+                    RegistrationTime = datereader.GetDateTime("registrationtime")
+                };
+                data= new { message = "Sikeres lekérdezés", result = blogger };
+
+            }
+            else
+            {
+               data= new { message = "Nincs ilyen blogger", result = "" };
+            }
+            connection.Close();
+            return data;
+        }
+        
+    }
+}
---
+++ BlogApi/Models/BlogPost.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata;
+
+namespace BlogApi.Models
+{
+    public class BlogPost
+    {
+        public string Title { get; set; }
+        public string Content { get; set; } = string.Empty;
+        public DateTime postTime { get; set; }
+
+        public DateTime updateTime { get; set; }
+        public int blogId { get; set; }
+    }
+
+}
